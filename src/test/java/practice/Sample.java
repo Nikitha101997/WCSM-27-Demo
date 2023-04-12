a1a30555549e516f58ed96cce305a9565ea4dbee
Nikitha101997 <nikithab946@gmail.com>
2023-04-12 20:42:11 +0530

    demo commited

---
+++ src/test/java/practice/Sample.java
@@ -0,0 +1,21 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+
+	
+		
+		@Test
+		
+		public void demo()
+		{
+			System.out.println("This is a demo");
+			
+			System.out.println("This a gitcommit");
+		}
+
+	}
+
+
